fa6d91da132928550f1c01c81b3584357ac7752c
jcybertech <covingtonjoshua7@gmail.com>
2020-11-04 21:35:32 -0700

    Working on understanding the use of State for variables that can be changed. Using this to change the amount of items that go in and out of the cart.

---
+++ src/Subtotal.js
@@ -0,0 +1,37 @@
+// const { FilterCenterFocus, ShoppingBasket } = require("@material-ui/icons");
+
+import React from "react";
+import CurrencyFormat from "react-currency-format";
+import "./Subtotal.css";
+import { useStateValue } from "./StateProvider";
+
+function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket.length} items):
+              <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+}
+
+export default Subtotal;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAkcpc0K418u7I_CQ6WxXsrMlDP20sQIRQ",
+    authDomain: "clone-a9441.firebaseapp.com",
+    databaseURL: "https://clone-a9441.firebaseio.com",
+    projectId: "clone-a9441",
+    storageBucket: "clone-a9441.appspot.com",
+    messagingSenderId: "494863882749",
+    appId: "1:494863882749:web:5d1b0b6d88e2e0d008a194",
+    measurementId: "G-M1XXJP5J1W"
+  };
