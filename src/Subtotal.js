e8f29176c04b663c18accc22f222cc2a88360673
jcybertech <covingtonjoshua7@gmail.com>
2020-11-04 22:01:03 -0700

    Working on the basket and check out page. Creating a selector to tally up the total price within the back.

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -4,6 +4,7 @@ import React from "react";
 import CurrencyFormat from "react-currency-format";
 import "./Subtotal.css";
 import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
@@ -15,7 +16,7 @@ function Subtotal() {
           <>
             <p>
               Subtotal ({basket.length} items):
-              <strong>0</strong>
+              <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -23,7 +24,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
